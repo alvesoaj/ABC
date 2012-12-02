afab0bab353a8b72bbddfbfd0b6e1b713ec068e3
AJ Alves <aj.alves@zerokol.com>
2012-12-02 20:12:30 -0300

    função 4

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,19 @@
 using namespace std;
 
 // BUG Eclipse
-// #define CLOCKS_PER_SEC 1000000
-
+#define CLOCKS_PER_SEC 1000000
 /* Parâmetros do algoritmo */
-#define POPULATION_SIZE 25
+#define PI 3.14159265
+#define POPULATION_SIZE 4
 #define FOOD_SOURCES_SIZE POPULATION_SIZE/2
 #define LIMIT (POPULATION_SIZE*PARAMS_SIZE)/2
-#define MAX_NUM_CYCLES 1000
-#define MAX_ITERATIONS 30
-/* Parâmetros do problema */
+#define MAX_NUM_CYCLES 100
+#define MAX_ITERATIONS 150
 #define PARAMS_SIZE 2
 #define UPPER_BOUND 1
 #define LOWER_BOUND 0
 
-double bounds_matrix[PARAMS_SIZE][2] = { { -20, 40 }, { -30, 50 } };
+double bounds_matrix[PARAMS_SIZE][2] = { { -5, 5 }, { -4, 8 } };
 double foods_matrix[FOOD_SOURCES_SIZE][PARAMS_SIZE];
 double function_array[FOOD_SOURCES_SIZE];
 double fitness_array[FOOD_SOURCES_SIZE];
@@ -68,8 +67,8 @@ int main(int argc, char *argv[]) {
 		}
 		string temp = "Iteração(" + number_to_String(iteration) + ")-> ";
 		for (int j = 0; j < PARAMS_SIZE; j++) {
-			temp += "X(" + number_to_String(j + 1) + ")=" + number_to_String(
-					optimun_params_array[j]) + " ";
+			temp += "X(" + number_to_String(j + 1) + ")="
+					+ number_to_String(optimun_params_array[j]) + " ";
 		}
 		cout << temp << endl;
 		iteration++;
@@ -77,8 +76,8 @@ int main(int argc, char *argv[]) {
 
 	cout << "f(x, y) = " << optimum_solution << endl;
 
-	cout << "Tempo de exec (ABC): " << calculate_time(time_start, clock()) << " ms"
-			<< endl;
+	cout << "Tempo de exec (ABC): " << calculate_time(time_start, clock())
+			<< " ms" << endl;
 	return 0;
 }
 
@@ -120,9 +119,13 @@ double calculate_function(double solution[PARAMS_SIZE]) {
 	 // MIN f(x, y) = (x-2)^4 + (x - 2y)^2
 	 return pow(solution[0] - 2, 4) + pow(solution[0] - 2 * solution[1], 2);
 	 */
-	// MIN f(x,y) = 100*(y-x^2)^2+(1 -x)^2
-	return 100 * pow(solution[1] - pow(solution[0], 2), 2) + pow(
-			1 - solution[0], 2);
+	/*
+	 // MIN f(x,y) = 100*(y-x^2)^2+(1 -x)^2
+	 return 100 * pow(solution[1] - pow(solution[0], 2), 2) + pow(
+	 1 - solution[0], 2);
+	 */
+	// MAX f(x) = x * sen(10*PI*x) + 1
+	return solution[0] * sin(10 * PI * solution[0]) + 1;
 }
 
 double calculate_fitness(double value) {
@@ -141,9 +144,11 @@ void init_bee(int index) {
 	double solution[PARAMS_SIZE];
 	for (int j = 0; j < PARAMS_SIZE; j++) {
 		double r = get_random_number();
-		foods_matrix[index][j] = r * (bounds_matrix[j][UPPER_BOUND]
-				- bounds_matrix[j][LOWER_BOUND])
-				+ bounds_matrix[j][LOWER_BOUND];
+		foods_matrix[index][j] =
+				r
+						* (bounds_matrix[j][UPPER_BOUND]
+								- bounds_matrix[j][LOWER_BOUND])
+						+ bounds_matrix[j][LOWER_BOUND];
 		solution[j] = foods_matrix[index][j];
 	}
 	function_array[index] = calculate_function(solution);
@@ -191,13 +196,13 @@ void send_employed_bees() {
 		/* se ultrapassar os limites*/
 		if (new_solution[param_to_modify]
 				< bounds_matrix[param_to_modify][LOWER_BOUND]) {
-			new_solution[param_to_modify]
-					= bounds_matrix[param_to_modify][LOWER_BOUND];
+			new_solution[param_to_modify] =
+					bounds_matrix[param_to_modify][LOWER_BOUND];
 		}
 		if (new_solution[param_to_modify]
 				> bounds_matrix[param_to_modify][UPPER_BOUND]) {
-			new_solution[param_to_modify]
-					= bounds_matrix[param_to_modify][UPPER_BOUND];
+			new_solution[param_to_modify] =
+					bounds_matrix[param_to_modify][UPPER_BOUND];
 		}
 
 		double new_solution_function = calculate_function(new_solution);
@@ -261,19 +266,19 @@ void send_onlooker_bees() {
 			r = get_random_number();
 			new_solution[param_to_modify] = foods_matrix[i][param_to_modify]
 					+ (foods_matrix[i][param_to_modify]
-							- foods_matrix[neighbour][param_to_modify]) * (r
-							- 0.5) * 2;
+							- foods_matrix[neighbour][param_to_modify])
+							* (r - 0.5) * 2;
 
 			/* se ultrapassar os limites*/
 			if (new_solution[param_to_modify]
 					< bounds_matrix[param_to_modify][LOWER_BOUND]) {
-				new_solution[param_to_modify]
-						= bounds_matrix[param_to_modify][LOWER_BOUND];
+				new_solution[param_to_modify] =
+						bounds_matrix[param_to_modify][LOWER_BOUND];
 			}
 			if (new_solution[param_to_modify]
 					> bounds_matrix[param_to_modify][UPPER_BOUND]) {
-				new_solution[param_to_modify]
-						= bounds_matrix[param_to_modify][UPPER_BOUND];
+				new_solution[param_to_modify] =
+						bounds_matrix[param_to_modify][UPPER_BOUND];
 			}
 
 			double new_solution_function = calculate_function(new_solution);
